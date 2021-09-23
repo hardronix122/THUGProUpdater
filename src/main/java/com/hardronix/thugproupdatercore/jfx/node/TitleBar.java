a542908ddd2855ed5feeae741a30a447e4dfc2af
hardronix122 <comparator657@gmail.com>
2021-09-23 17:58:37 +0300

    Add Close Button

--- src/main/java/com/hardronix/thugproupdatercore/jfx/node/TitleBar.java
+++ src/main/java/com/hardronix/thugproupdatercore/jfx/node/TitleBar.java
@@ -1,26 +1,36 @@
 package com.hardronix.thugproupdatercore.jfx.node;
 
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 
 import java.io.InputStream;
 
-public class TitleBar extends VBox {
+public class TitleBar extends StackPane {
 
 	public TitleBar(String title, int width) {
 		this.setBackground(new Background(new BackgroundFill(Paint.valueOf("#181818"), CornerRadii.EMPTY, Insets.EMPTY)));
-		this.setMinWidth(width);
-		this.setMinHeight(48);
+		this.setPrefSize(width, 48);
 		this.setAlignment(Pos.CENTER);
+
+		HBox closeButtonBox = new HBox();
+		closeButtonBox.setAlignment(Pos.TOP_RIGHT);
+		closeButtonBox.setPadding(new Insets(4, 4, 4, 4));
+
+		Button closeButton = new Button();
+		closeButton.setId("close-button");
+		closeButton.setOnAction(e -> Platform.exit());
+
+		closeButtonBox.getChildren().add(closeButton);
+		this.getChildren().add(closeButtonBox);
+
 
 		Label label = new Label(title);
 		label.setAlignment(Pos.CENTER);
